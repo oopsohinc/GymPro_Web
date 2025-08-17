f4c41dc1b912095925fee995231cf2619dab6f9a
oopsohinc <minhnguyen.160479@gmail.com>
2025-08-17 21:45:31 +0700

    Thêm luồng thanh toán trực tuyến

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sql = require('mssql');
 const config = require('../db');
+const { VNPay, ignoreLogger, ProductCode, VnpLocale, dateFormat } = require('vnpay');
 
 // Lấy danh sách thanh toán
 router.get('/payments', async (req, res) => {
@@ -19,6 +20,35 @@ router.get('/payments', async (req, res) => {
         console.error('Error fetching payments:', err);
         res.status(500).send('Server error');
     }
+});
+
+// Thanh toán trực tiếp
+router.post('/payments/create-qr', async (req, res) => {
+    const vnpay = new VNPay({
+        tmnCode: 'YXC7JZLY',
+        secureSecret: '7CYIPMPBQJ1N84RIMRBDT37IKIHPHRYF',
+        vnpayHost: 'https://sandbox.vnpayment.vn',
+        testMode: true,
+        hashAlgorithm: 'SHA512',
+        loggerFn: ignoreLogger
+    });
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const vnpayResponse = await vnpay.buildPaymentUrl({
+        vnp_Amount: 50000,
+        vnp_IpAddr: '127.0.0.1',
+        vnp_TxnRef: 'txn001',
+        vnp_OrderInfo: 'Payment for order 001',
+        vnp_OrderType: ProductCode.Other,
+        vnp_ReturnUrl: 'http://localhost:3000/api/payments/callback',
+        vnp_Locale: VnpLocale.VN,
+        vnp_CreateDate: dateFormat(new Date(), 'yyyyMMddHHmmss'),
+        vnp_ExpireDate: dateFormat(tomorrow)
+    });
+
+    return res.status(200).json({ vnpayResponse });
 });
 
 module.exports = router;
